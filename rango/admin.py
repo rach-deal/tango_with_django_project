9b71f97e2ac9750c44ac9568f00a79e6885cd125
rach-deal <rachdeal@gmail.com>
2022-02-02 20:38:31 +0000

    Chapter 5 Without Exercises

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,8 @@
+# Import model before registering
+
 from django.contrib import admin
+from rango.models import Category, Page
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Page)
--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Primary Key already provided (id = auto-inc integer field)
+
+class Category(models.Model):
+	name = models.CharField(max_length = 128, unique = True)
+
+	class Meta:
+		verbose_name_plural = 'Categories'
+
+	def __str__(self):
+		return self.name
+
+class Page(models.Model):
+	category = models.ForeignKey(Category, on_delete = models.CASCADE)
+	title = models.CharField(max_length = 128)
+	url = models.URLField()
+	views = models.IntegerField(default = 0)
+
+	# URL Field same as Char Field, can spec max length
+	# DateField stores a Python datetime.date object
+
+	def __str__(self):
+		return self.title
